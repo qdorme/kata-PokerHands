a4a2e0921946e1177526b0f9cadd664727f29364
Quentin <qdorme@gmail.com>
2021-10-03 20:29:53 +0200

    [TEST] Verify entry data

--- src/test/java/qdo/kata/CardTest.java
+++ src/test/java/qdo/kata/CardTest.java
@@ -3,6 +3,7 @@ package qdo.kata;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class CardTest {
 
@@ -10,5 +11,13 @@ class CardTest {
 	public void shouldCreateACard(){
 		assertThat(Card.from("2C")).isInstanceOf(Card.class);
 		assertThat(Card.from("2c")).isInstanceOf(Card.class);
+	}
+
+	@Test
+	public void shouldThrowIllegalArgumentException(){
+		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Card.from(""));
+		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Card.from("2"));
+		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Card.from("C"));
+		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Card.from("2CS"));
 	}
 }
