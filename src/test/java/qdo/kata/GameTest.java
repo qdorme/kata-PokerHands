c3c6f42a1453202c9429a57364f5dd7333576f63
Quentin <qdorme@gmail.com>
2021-10-03 20:45:32 +0200

    [TEST] verify that a card is picked only once

--- src/test/java/qdo/kata/GameTest.java
+++ src/test/java/qdo/kata/GameTest.java
@@ -9,5 +9,6 @@ class GameTest {
 	@Test
 	public void shouldThrowIllegalArgumentException(){
 		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Game.from("Black: 3D 5S 9C KD : 2H 3H 4S 8C AH"));
+		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Game.from("Black: 3D 5S 9C KD 2C White : 2C 3H 4S 8C AH"));
 	}
 }
