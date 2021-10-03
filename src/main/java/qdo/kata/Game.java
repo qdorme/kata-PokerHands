61041cfb15815edbec23d97496d3f58c5c0bf010
Quentin <qdorme@gmail.com>
2021-10-03 21:05:46 +0200

    [TEST] Create a game with 2 players - OK

--- src/main/java/qdo/kata/Game.java
+++ src/main/java/qdo/kata/Game.java
@@ -12,6 +12,12 @@ public class Game {
 	private static final Pattern VALID_SETTINGS = Pattern.compile(
 			"\\s*(\\w+)\\s*:\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w+)\\s*:\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*");
 
+	private final List<Player> players;
+
+	private Game(List<Player> players){
+		this.players=players;
+	}
+
 	public static Game from(String dataEntries) {
 		Matcher settingsEntries = VALID_SETTINGS.matcher(dataEntries);
 		if (!settingsEntries.matches())
@@ -22,12 +28,18 @@ public class Game {
 		List<Card> secondPlayerCards = Arrays.asList(Card.from(settingsEntries.group(8)), Card.from(settingsEntries.group(9)),
 				Card.from(settingsEntries.group(10)), Card.from(settingsEntries.group(11)), Card.from(settingsEntries.group(12)));
 		verifyThatACardIsPickedOnlyOnce(firstPlayerCards, secondPlayerCards);
-		return new Game();
+		return new Game(Arrays.asList(new Player(settingsEntries.group(1),firstPlayerCards),new Player(settingsEntries.group(7),secondPlayerCards)));
 	}
+
+
 
 	private static void verifyThatACardIsPickedOnlyOnce(List<Card> firstPlayerCards, List<Card> secondPlayerCards) {
 		if(Stream.concat(firstPlayerCards.stream(), secondPlayerCards.stream()).collect(Collectors.groupingBy(Card::name))
 				.entrySet().stream().filter(set->set.getValue().size() != 1).count() > 0)
 			throw new IllegalArgumentException("A card can not be picked twice");
+	}
+
+	public List<Player> players() {
+		return players;
 	}
 }
--- src/main/java/qdo/kata/Player.java
+++ src/main/java/qdo/kata/Player.java
@@ -1,5 +1,22 @@
 package qdo.kata;
 
+import java.util.List;
+
 public class Player {
 
+	private final String name;
+	private final List<Card> hand;
+
+	public Player(String name, List<Card> hand) {
+		this.name = name;
+		this.hand = hand;
+	}
+
+	public String name() {
+		return name;
+	}
+
+	public List<Card> hand() {
+		return hand;
+	}
 }
