b05bffccdde62f9ecca940afbba99d9a9ad52642
Quentin <qdorme@gmail.com>
2021-10-03 20:19:08 +0200

    [TEST] Each Player should have 5 cards and a name - OK

--- src/main/java/qdo/kata/Game.java
+++ src/main/java/qdo/kata/Game.java
@@ -1,8 +1,17 @@
 package qdo.kata;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Game {
+	private static final Pattern VALID_SETTINGS = Pattern.compile(
+			"\\s*(\\w+)\\s*:\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w+)\\s*:\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*");
 
 	public static Game from(String dataEntries) {
+		Matcher settingsEntries = VALID_SETTINGS.matcher(dataEntries);
+		if (!settingsEntries.matches())
+			throw new IllegalArgumentException("Data provided are not well setted");
+
 		return null;
 	}
 }
