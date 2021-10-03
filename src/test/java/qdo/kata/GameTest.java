4e32df4c8860e188501820d3559ee72a1e987e6b
Quentin <qdorme@gmail.com>
2021-10-03 20:54:24 +0200

    [TEST] Create a game

--- src/test/java/qdo/kata/GameTest.java
+++ src/test/java/qdo/kata/GameTest.java
@@ -2,6 +2,7 @@ package qdo.kata;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class GameTest {
@@ -10,5 +11,10 @@ class GameTest {
 	public void shouldThrowIllegalArgumentException(){
 		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Game.from("Black: 3D 5S 9C KD : 2H 3H 4S 8C AH"));
 		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Game.from("Black: 3D 5S 9C KD 2C White : 2C 3H 4S 8C AH"));
+	}
+
+	@Test
+	public void shouldCreateAGame(){
+		assertThat(Game.from("Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH")).isInstanceOf(Game.class);
 	}
 }
