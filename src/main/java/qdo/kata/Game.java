7b621c049ce7f9472fa56a41e8824e0b3149dbdb
Quentin <qdorme@gmail.com>
2021-10-03 20:48:27 +0200

    [TEST] verify that a card is picked only once - OK

--- src/main/java/qdo/kata/Game.java
+++ src/main/java/qdo/kata/Game.java
@@ -1,9 +1,14 @@
 package qdo.kata;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Game {
+
 	private static final Pattern VALID_SETTINGS = Pattern.compile(
 			"\\s*(\\w+)\\s*:\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w+)\\s*:\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*(\\w{2})\\s*");
 
@@ -12,6 +17,13 @@ public class Game {
 		if (!settingsEntries.matches())
 			throw new IllegalArgumentException("Data provided are not well setted");
 
+		List<Card> firstPlayerCards = Arrays.asList(Card.from(settingsEntries.group(2)), Card.from(settingsEntries.group(3)),
+				Card.from(settingsEntries.group(4)), Card.from(settingsEntries.group(5)), Card.from(settingsEntries.group(6)));
+		List<Card> secondPlayerCards = Arrays.asList(Card.from(settingsEntries.group(8)), Card.from(settingsEntries.group(9)),
+				Card.from(settingsEntries.group(10)), Card.from(settingsEntries.group(11)), Card.from(settingsEntries.group(12)));
+		if(Stream.concat(firstPlayerCards.stream(), secondPlayerCards.stream()).collect(Collectors.groupingBy(Card::name))
+				.entrySet().stream().filter(set->set.getValue().size() != 1).count() > 0)
+			throw new IllegalArgumentException("A card can not be picked twice");
 		return null;
 	}
 }
